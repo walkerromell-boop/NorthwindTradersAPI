67bff988f57ac08b49aab7798fecfed68f8af6d6
Romell Walker <walkerromell@gmail.com>
2025-12-11 15:08:17 -0500

    add product dao

---
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/data/ProductDao.java
@@ -0,0 +1,7 @@
+package com.pluralsight.NorthwindTradersAPI.data;
+
+public interface ProductDao {
+    List<Product> getAll();
+    Product getById(int id);
+
+}
---
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/model/ProductN.java
@@ -0,0 +1,124 @@
+package com.pluralsight.NorthwindTradersAPI.model;
+
+
+public class ProductN {
+    private int productId;        // auto-generated
+    private String productName;
+    private int supplierId;
+    private int categoryId;
+    private String quantityPerUnit;
+    private double unitPrice;
+    private short unitsInStock;
+    private short unitsOnOrder;
+    private short reorderLevel;
+    private boolean discontinued;
+
+    public ProductN(Integer productId, String productName, Integer supplierId, Integer categoryId, String quantityPerUnit, Double unitPrice, Short unitsInStock, Short unitsOnOrder, Short reorderLevel, Boolean discontinued) {
+        this.productId = productId;
+        this.productName = productName;
+        this.supplierId = supplierId;
+        this.categoryId = categoryId;
+        this.quantityPerUnit = quantityPerUnit;
+        this.unitPrice = unitPrice;
+        this.unitsInStock = unitsInStock;
+        this.unitsOnOrder = unitsOnOrder;
+        this.reorderLevel = reorderLevel;
+        this.discontinued = discontinued;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(Integer supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getQuantityPerUnit() {
+        return quantityPerUnit;
+    }
+
+    public void setQuantityPerUnit(String quantityPerUnit) {
+        this.quantityPerUnit = quantityPerUnit;
+    }
+
+    public Double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Short getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(Short unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public Short getUnitsOnOrder() {
+        return unitsOnOrder;
+    }
+
+    public void setUnitsOnOrder(Short unitsOnOrder) {
+        this.unitsOnOrder = unitsOnOrder;
+    }
+
+    public Short getReorderLevel() {
+        return reorderLevel;
+    }
+
+    public void setReorderLevel(Short reorderLevel) {
+        this.reorderLevel = reorderLevel;
+    }
+
+    public Boolean getDiscontinued() {
+        return discontinued;
+    }
+
+    public void setDiscontinued(Boolean discontinued) {
+        this.discontinued = discontinued;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", supplierId=" + supplierId +
+                ", categoryId=" + categoryId +
+                ", quantityPerUnit='" + quantityPerUnit + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", unitsInStock=" + unitsInStock +
+                ", unitsOnOrder=" + unitsOnOrder +
+                ", reorderLevel=" + reorderLevel +
+                ", discontinued=" + discontinued +
+                '}';
+    }
+}
