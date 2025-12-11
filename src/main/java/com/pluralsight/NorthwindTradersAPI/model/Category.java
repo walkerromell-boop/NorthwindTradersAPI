169801296dc64da62c68f794ba688977c0243f67
Romell Walker <walkerromell@gmail.com>
2025-12-11 13:11:17 -0500

    local host working

---
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/model/Category.java
@@ -0,0 +1,37 @@
+package com.pluralsight.NorthwindTradersAPI.model;
+
+public class Category {
+    private int categoryId;
+    private String categoryName;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Category(int categoryId,String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", categoryName='" + categoryName + '\'' +
+                '}';
+    }
+}
